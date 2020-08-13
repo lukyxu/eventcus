2eda3296c3a34727dc9a01faebaf2b890940b47d
Alex <axl18@ic.ac.uk>
2020-08-13 21:55:25 +0100

    Create navbar on dashboard page and add css file

--- client/src/pages/create-event.js
+++ client/src/pages/create-event.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Create extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Create Event</h2>
-          <p>this is the create event page</p>
-          <Link to={'/'} className="nav-link"> Dashboard </Link>
-        </div>
-    );
-  }
-}
-
-export default Create;
+export default function Create() {
+        return (
+            <div>
+                <h2>Create Event</h2>
+                <p>this is the create event page</p>
+                <Link to={'/'} className="nav-link"> Dashboard </Link>
+            </div>
+        );
+    }
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles.css';
+import {Navbar} from 'react-bootstrap';
 
-class Dashboard extends Component {
-  render() {
+export default function Dashboard() {
     return (
+
         <div>
-          <h2>Dashboard</h2>
-          <p>this is the dashboard page</p>
-          <Link to={'/create-event'} className="nav-link">Create Event</Link>
+            <Navbar fixed='top' variant='dark'>
+                Dashboard
+            </Navbar>
+            <div>
+                <h2>Dashboard</h2>
+                <p>this is the dashboard page</p>
+                <Link to={'/create-event'} className="nav-link">Create Event</Link>
+            </div>
         </div>
     );
-  }
 }
-
-export default Dashboard;
