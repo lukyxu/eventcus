1dd490cc0818f93babfb9b124589c35cdbb9b2ea
Alex <axl18@ic.ac.uk>
2020-08-14 17:02:44 +0100

    Create create-event page skeleton and event-form

--- client/src/components/event-form.js
+++ client/src/components/event-form.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Header from './header.js'
-import { Form, Col, Row } from 'react-bootstrap';
-import EventForm from './../components/event-form';
+import { Form, Col, Row, Container, FormCheck } from 'react-bootstrap';
 
 export default function EventForm() {
 
@@ -10,29 +7,158 @@ export default function EventForm() {
     const [date, setDate] = useState('');
     const [details, setDetails] = useState('');
     const [release, setRelease] = useState('');
+    const [payment, setPayment] = useState('');
+
+    const [defaultFieldsChecked, setDefaults] = useState({
+        'Full Name': true,
+        'Shortcode': true,
+        'Email': true,
+        'Contact Number': true,
+        'Food Allergy': false,
+    });
+    const defaultFields = ['Full Name', 'Shortcode', 'Email', 'Contact Number', 'Food Allergy'];
+
+    const [ticketTypes, setTicketTypes] = useState([{type: '', price: 0}]);
 
     const handleSubmit = (event) => { };
-    
+
     return (
-        <Form on submit={handleSubmit}>
-            <Form.Group as={Row}>
-                <Form.Group as={Row}>
-                    <Col>
-                        <Form.Label>Event Name</Form.Label>
-                    </Col>
-                    <Col>
-                        <Form.Control placeholder="Enter an event name" value={name} onChange={(e) => setName(e.target.value)} />
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row}>
-                    <Col>
-                        <Form.Label>Event Date</Form.Label>
-                    </Col>
-                    <Col>
-                        <Form.Control placeholder="Enter an event date" value={name} onChange={(e) => setDate(e.target.value)} />
-                    </Col>
-                </Form.Group>
-            </Form.Group>
-        </Form>
+        <div className='eventFormMain'>
+            <Form on submit={handleSubmit}>
+                <h1>Event Information</h1>
+                <div className='eventFormSection'>
+                    <Row>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Event Name</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Enter an event name" value={name} onChange={(e) => setName(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Event Date</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Choose date and time" value={name} onChange={(e) => setDate(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Event Details</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control as="textarea" placeholder="Enter some details" value={name} rows='3' onChange={(e) => setDetails(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Ticket Release</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Choose date and time" value={name} onChange={(e) => setRelease(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Payment Info</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Payment information" value={name} onChange={(e) => setPayment(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </div>
+                <br />
+                <h1>Ticket Information</h1>
+
+                <div className='eventFormSection'>
+                    <Row>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Event Name</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Enter an event name" value={name} onChange={(e) => setName(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Event Date</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Choose date and time" value={name} onChange={(e) => setDate(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Event Details</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control as="textarea" placeholder="Enter some details" value={name} rows='3' onChange={(e) => setDetails(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Ticket Release</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Choose date and time" value={name} onChange={(e) => setRelease(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Col>
+                                    <Form.Label>Payment Info</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control placeholder="Payment information" value={name} onChange={(e) => setPayment(e.target.value)} />
+                                </Col>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </div>
+
+                <br />
+                <h1>Sign Up Form</h1>
+
+                <div className='eventFormSection'>
+                    <Row>
+                        <Form.Group>
+                            <Form.Label>
+                                Select default fields to add to sign up form
+                            </Form.Label>
+                            {defaultFields.map(t => (
+                                <Form.Check key={t} checked={defaultFieldsChecked[t]} type="checkbox" label={t} id={"checkbox_" + t}
+                                    onChange={() => {
+                                        defaultFieldsChecked[t] = !defaultFieldsChecked[t]
+                                        setDefaults({ ...defaultFieldsChecked })
+                                    }} />
+                            ))}
+                        </Form.Group>
+                    </Row>
+                </div>
+            </Form>
+        </div>
+
     );
 }
--- client/src/pages/create-event.js
+++ client/src/pages/create-event.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './../components/header.js'
-import { Form, Col, Row } from 'react-bootstrap';
-
+import EventForm from './../components/event-form';
 
 export default function Create() {
-
-    const [name, setName] = useState('');
-    const [date, setDate] = useState('');
-    const [details, setDetails] = useState('');
-    const [release, setRelease] = useState('');
-
-    const handleSubmit = (event) => { };
-
     return (
         <div>
             <Header title='Create Event' />
             <Link to={'/'} className="nav-link"> Dashboard </Link>
-            
+            <EventForm/>
+
         </div>
     );
 }
