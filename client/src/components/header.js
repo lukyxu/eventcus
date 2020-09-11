c606bb037ab8187544814d50bdf679db8ce2a3c0
Alex <alexliu0002@gmail.com>
2020-09-11 17:26:35 +0100

    Change header to use logo

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,11 +8,18 @@ import { toast } from 'react-toastify';
 export default function Header({ title, setUser }) {
   const history = useHistory();
   return <Navbar variant="dark" sticky="top">
-      <Navbar.Brand>{title}</Navbar.Brand>
+
+      <Navbar.Brand style={{alignItems:"center"}}> 
+      <Nav.Link as={Link} to="/"><img src="./../../assets/eventcus-logo-white-on-blue.png" width="42" height="42" class="d-inline-block align-top" alt=""/></Nav.Link>
+      </Navbar.Brand>
+      <Nav className="navbarTitle mr-auto" >
+          {title}
+      </Nav>
+      {/* <Navbar.Brand>{title}</Navbar.Brand>
       <Nav className="mr-auto">
           {title === "Dashboard" ? null
               : <Nav.Link as={Link} to="/">Dashboard</Nav.Link> }
-      </Nav>
+      </Nav> */}
       <Nav className="justify-content-end">
           <Button variant="outline-light" onClick={() => { logout((res) => { 
             if (res.error) {
